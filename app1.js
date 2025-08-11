e5a4436594cc5d15ddcb86fd10d8195f3e08b438
Nithin Prakash <nithinpnandanam@gmail.com>
2025-08-11 17:10:10 +0530

    undefined

---
+++ app1.js
@@ -0,0 +1,15 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+var a ;
+console.log(a===undefined)
+// undefined is a keyword in js
+// so its better not to use it for value assignment  
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+let b ;
+// if we check the sources tab while inspecting ,and particularly in the scope section we can see that this variable b will not be having the value undefined in the global scope
+// Instead it wll be showing <value unavailable> in the "scrpit" scope
+// after that line is run or initialised the  the value changes to undefined
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
